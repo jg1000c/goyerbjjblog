d0b0eee0d1bfcba1d9b6abd6fbea72233a9bced5
Jason <jg1000c@gmail.com>
2018-01-05 15:24:10 -0500

    first commit

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+from embed_video.fields import EmbedVideoField
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    video = EmbedVideoField(verbose_name='My video',
+                            help_text='This is a help text')
+
+    def __unicode__(self):
+        return self.title
+
+
+    def __unicode__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'pk': self.pk})
