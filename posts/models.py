73ba5f4fb6f39140c0ad9543408032401b05b74c
Jason <jg1000c@gmail.com>
2018-01-09 21:00:20 -0500

    updated

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ from embed_video.fields import EmbedVideoField
 class Post(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
     video = EmbedVideoField(verbose_name='My video',
                             help_text='This is a help text')
 
@@ -20,5 +21,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'pk': self.pk})
 
-
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 
