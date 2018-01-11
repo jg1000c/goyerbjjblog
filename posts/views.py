eaa3aaf12b1d96aa0dbd5bbb24d0762198415942
Jason <jg1000c@gmail.com>
2018-01-11 18:47:41 -0500

    changes

--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,9 @@ class PostListView(ListView):
     model = Post
     paginate_by = 10
 
+    def get_queryset(self):
+        queryset = Post.objects.order_by('-pub_date')
+        return queryset
 
 class PostDetailView(DetailView):
     model = Post
